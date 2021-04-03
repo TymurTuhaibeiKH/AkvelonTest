331145374b7070bf1d919589cd383263791ad4e5
Tymur Tuhaibei <Tymur_Tuhaibei@epam.com>
2021-04-03 10:58:02 +0300

    Initial commit

---
+++ src/main/java/com/tuhaibei/akvelonTest/AkvelonTestApplication.java
@@ -0,0 +1,13 @@
+package com.tuhaibei.akvelonTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkvelonTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkvelonTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuhaibei/akvelonTest/AkvelonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuhaibei.akvelonTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkvelonTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
